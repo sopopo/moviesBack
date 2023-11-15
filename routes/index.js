26f0ab999c048a8dbc2b6f714ae840d39fe2cf09
Sophie Blanchon <sblanchon@gmail.com>
2023-11-15 14:24:07 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = router;
+
+const apiMovie = process.env.API_MOVIE;
+
+router.get('/movies', (req, res) => {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key='+apiMovie)
+      .then(response => response.json())
+      .then(data => {
+        res.json({ movies: data.results });
+      });
+   });
