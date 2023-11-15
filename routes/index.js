472417bbc68a5a94557f50d266b2dcbc3ebd54b9
Sophie Blanchon <sblanchon@gmail.com>
2023-11-15 15:01:22 +0100

    adjust route with back

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ module.exports = router;
 
 const apiMovie = process.env.API_MOVIE;
 
-router.get('/movies', (req, res) => {
+router.get('https://movies-back-beta.vercel.app/movies', (req, res) => {
     fetch('https://api.themoviedb.org/3/discover/movie?api_key='+apiMovie)
       .then(response => response.json())
       .then(data => {
